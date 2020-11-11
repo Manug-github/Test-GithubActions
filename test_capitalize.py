7e6deb86cfed3a8d1ea5aa19d8a5342d5e4e22bc
Manug-local <34220384+Manug-github@users.noreply.github.com>
2020-11-11 20:28:54 +0100

    First commit, capitalize.

---
+++ test_capitalize.py
@@ -0,0 +1,11 @@
+# test_capitalize.py
+
+def capitalize_string(s):
+    if not isinstance(s, str):
+        raise TypeError('Please provide a string')
+    return s.capitalize()
+
+def test_capitalize_string():
+    assert capitalize_string('test') == 'Test'
+    assert capitalize_string('TEST') == 'Test'
+    assert capitalize_string('tEST') == 'Test'
